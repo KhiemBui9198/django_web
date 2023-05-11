d7f3fd60f5e5362084293f9f06ac307fcb15bcc7
Khiembui <chikhiemkm9198@gmail.com>
2023-05-11 15:26:16 +0700

    Implement_book_view

---
+++ django_web/catalog/urls.py
@@ -0,0 +1,11 @@
+# Use include() to add paths from the catalog application
+from django.urls import include
+from django.urls import path
+from django.views.generic import RedirectView
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('books/', views.BookListView.as_view(), name='books'),
+    path('book/<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
+]
